5c3f38db61a11d40418afadf6f62bd36ccd6f4e0
aevix <55609333+aevix@users.noreply.github.com>
2020-02-03 03:31:47 -0600

    add incoming tab

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,11 @@ from app import app, db
 
 
 @app.route('/')
-@app.route('/home_template')
+@app.route('/home_template', methods=['GET'])
 def Home():
     return render_template('home_template.html', title='Home')
+
+@app.route('/Incoming')
+def Incoming():
+    return render_template('Incoming.html', title='Incoming')
+
