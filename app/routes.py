c682ee0f4244c5f4f9a775a85153d0e84bc24782
aevix <55609333+aevix@users.noreply.github.com>
2020-02-03 20:48:05 -0600

    add outgoing and new inventory tab

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, flash
 from app import app, db
 
-
 @app.route('/')
 @app.route('/home_template', methods=['GET'])
 def Home():
@@ -11,3 +10,10 @@ def Home():
 def Incoming():
     return render_template('Incoming.html', title='Incoming')
 
+@app.route('/Outgoing')
+def Outgoing():
+    return render_template('Outgoing.html', title='Outgoing')
+
+@app.route('/New_inventory')
+def New_inventory():
+    return render_template('New_inventory.html', title='New_inventory')
