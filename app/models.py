3dc2c2263ef0d9ea4849f57ba8965bb284b18a60
aevix <55609333+aevix@users.noreply.github.com>
2020-02-05 02:27:20 -0600

    add status table into db

--- app/models.py
+++ app/models.py
@@ -6,8 +6,16 @@ class Outgoing(db.Model):
     barcode = db.Column(db.String(20), index = True)
     item_type = db.Column(db.String(10), index = True)
     item_size = db.Column(db.String(10), index = True)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    status = db.Column(db.Boolean(), index=True)
+    time_stamps = db.relationship('Status', backref="item", lazy= 'dynamic')
 
     def __repr__(self):
-        return '<Item: {} {} {} {} {}>'.format(self.barcode, self.item_type, self.item_size, self.timestamp, self.status)
+        return '<Item: {} {} {}>'.format(self.barcode, self.item_type, self.item_size)
+
+class Status(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    status = db.Column(db.Boolean(), index=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('outgoing.id'))
+
+    def __repr__(self):
+        return '<Item: {}>'.format(self.timestamp)
