c808cb91b8eeefa59a83766b3991d99e68d17132
aevix <55609333+aevix@users.noreply.github.com>
2020-02-16 03:18:11 -0600

    update db table update incoming and outgoing html

--- app/models.py
+++ app/models.py
@@ -7,17 +7,18 @@ class Laundry(db.Model):
     barcode = db.Column(db.String(20), index = True)
     item_type = db.Column(db.String(10), index = True)
     item_size = db.Column(db.String(10), index = True)
-    time_stamps = db.relationship('Status', backref="item", lazy= 'dynamic')
+    item_note = db.Column(db.String(20), index = True)
+    status = db.Column(db.Boolean(), index=True)
+    time_stamps = db.relationship('Timestamp', backref="item", lazy= 'dynamic')
 
     def __repr__(self):
-        return '<Item: {} {} {}>'.format(self.barcode, self.item_type, self.item_size)
+        return '<Item: {} {} {} {} {}>'.format(self.barcode, self.item_type, self.item_size, self.item_note, self.status)
 
 
-class Status(db.Model):
+class Timestamp(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    status = db.Column(db.Boolean(), index=True)
     item_id = db.Column(db.Integer, db.ForeignKey('laundry.id'))
 
     def __repr__(self):
-        return '<Item: {}>'.format(self.timestamp)
+        return '<time: {}>'.format(self.timestamp)
